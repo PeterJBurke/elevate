0e4877479c71f6b0d3da36ed2d291988ff1eed1b
Peter Burke <peter.burke.john@gmail.com>
2023-12-30 16:29:17 -0800

    first commit

---
+++ crm/forms.py
@@ -0,0 +1,17 @@
+from dataclasses import field
+from turtle import textinput
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.forms.widgets import PasswordInput, TextInput
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2']
